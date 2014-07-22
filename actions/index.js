6d7c8c46b49a424612f6cfce75a259beb46f2a43
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-07-21 21:57:04 -0600

    Hello, gebo-proxy-action

---
+++ actions/index.js
@@ -0,0 +1,53 @@
+var performatives = require('gebo-performatives'),
+    nconf = require('nconf'),
+    q = require('q');
+
+module.exports = function() {
+
+    /**
+     * Spoof all the file _proxyActions set in the
+     * gebo.json file
+     */
+    nconf.file({ file: './gebo.json' });
+    var _proxyActions = nconf.get('proxyActions');
+    var keys = Object.keys(_proxyActions);
+    for (var i in keys) {
+      exports[keys[i]] = _forward;
+    }
+
+    /**
+     * Forward the message to the appropriate recipient
+     */
+    function _forward(verified, message) {
+        var deferred = q.defer();
+    
+        if (verified.admin || verified.execute) {
+
+          if (_proxyActions[message.action]) {
+            // Swap out destination gebo and access token
+            message.gebo = _proxyActions[message.action].gebo;
+            message.access_token = _proxyActions[message.action].access_token;
+  
+            performatives.request(message, function(err, results) {
+                  if (err) {
+                    deferred.reject(err);
+                  }
+                  else {
+                    deferred.resolve(results);
+                  }
+              });
+          }
+          else {
+            deferred.reject('I don\'t know how to ' + message.action);
+          }
+        }
+        else {
+          deferred.reject('You are not permitted to request or propose that action');
+        }
+        return deferred.promise;
+      };
+    exports.forward = _forward;
+
+    return exports;
+};
+
---
+++ test/index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var actionModule = require('..'),
+    performatives = require('gebo-performatives'),
+    q = require('q');
+
+/**
+ * onLoad
+ */
+exports.onLoad =  {
+
+    'Point all configured actions to the forward actions': function(test) {
+        test.expect(3);
+        test.equal(Object.keys(actionModule.actions).length, 3);
+        test.equal(actionModule.someAction, actionModule.forward);
+        test.equal(actionModule.someOtherAction, actionModule.forward);
+        test.done();
+    },
+};
+
+/**
+ * forward
+ */
+var _oldFunc;
+exports.forward = {
+    setUp: function(callback) {
+        _oldFunc = performatives.request;
+
+        // This stub simply passes the modified message back
+        // to the tests
+        performatives.request = function(message, done) {
+            done(null, message);
+          };
+        callback();
+    },
+
+    tearDown: function(callback) {
+        performatives.request = _oldFunc;
+        callback();
+    },
+
+    'Forward the message to the destination set in gebo.json': function(test) {
+        test.expect(4);
+        actionModule.actions.someAction(
+                { resource: 'someAction', execute: true },
+                {
+                    sender: 'someagent@example.com',
+                    performative: 'request',
+                    action: 'someAction',
+                    gebo: 'https://proxygebo.com',
+                    access_token: 'abc123',
+                }).
+            then(function(results) {
+                test.equal(results.gebo, 'https://somegebo.com');
+                test.equal(results.access_token, 'SomeAccessToken123');
+
+                // Try the other action
+                actionModule.actions.someOtherAction(
+                        { resource: 'someOtherAction', execute: true },
+                        {
+                            sender: 'someagent@example.com',
+                            performative: 'request',
+                            action: 'someOtherAction',
+                            gebo: 'https://proxygebo.com',
+                            access_token: 'abc123',
+                        }).
+                    then(function(results) {
+                        test.equal(results.gebo, 'https://someothergebo.com');
+                        test.equal(results.access_token, 'SomeOtherAccessToken123');
+                        test.done();
+                      }).
+                    catch(function(err) {
+                        test.ok(false, err);      
+                        test.done();
+                      });
+              }).
+            catch(function(err) {
+                test.ok(false, err);      
+                test.done();
+              });
+    },
+
+    // This test may be reduntant. Non-existent actions should be caught
+    // in the perform route
+    'Shouldn\'t barf if given an unspecified action to proxy': function(test) {
+        test.expect(1);
+        actionModule.actions.forward(
+                { resource: 'someNonExistentAction', execute: true },
+                {
+                    sender: 'someagent@example.com',
+                    performative: 'request',
+                    action: 'someNonExistentAction',
+                    gebo: 'https://proxygebo.com',
+                    access_token: 'abc123',
+                }).
+            then(function(results) {
+                test.ok(false, 'Shouldn\'t get here');      
+                test.done();
+              }).
+            catch(function(err) {
+                test.equal(err, 'I don\'t know how to someNonExistentAction');
+                test.done();
+              });
+    },
+};
