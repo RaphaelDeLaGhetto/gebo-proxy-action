578edece9a6bc76998f81e689960f82f75a49c3a
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-03-18 14:09:26 -0600

    DRY out and clean up

--- actions/index.js
+++ actions/index.js
@@ -36,32 +36,23 @@ module.exports = function() {
 
           if (_directory[message.receiver].resources && _directory[message.receiver].resources[verified.resource]) {
 
+            // Swap out destination gebo and access token
+            message.gebo = _directory[message.receiver].gebo;
+            message.access_token = _directory[message.receiver].access_token;
+            message.sender = _proxyEmail;
+ 
+            // The requesting agent isn't allowed to do anything yet
+            var permitted = false;
+
             // Get this proxy's permissions
             var proxyPermissions = _directory[message.receiver].resources[verified.resource];
 
             // Permissions have to be identical for actions
-            if (proxyPermissions.isAction) {
-              if (verified.read === !!proxyPermissions.read &&
-                  verified.write === !!proxyPermissions.write &&
-                  verified.execute === !!proxyPermissions.execute) {
-              
-                // Swap out destination gebo and access token
-                message.gebo = _directory[message.receiver].gebo;
-                message.access_token = _directory[message.receiver].access_token;
-                message.sender = _proxyEmail;
-    
-                performatives.request(message, function(err, results) {
-                      if (err) {
-                        deferred.reject(err);
-                      }
-                      else {
-                        deferred.resolve(results);
-                      }
-                  });
-              }
-              else {
-                deferred.reject('You are not permitted to request or propose that action');
-              }
+            if (proxyPermissions.isAction &&
+                verified.read === !!proxyPermissions.read &&
+                verified.write === !!proxyPermissions.write &&
+                verified.execute === !!proxyPermissions.execute) {
+              permitted = true;              
             }
             // Apply permissions according to requested action for database resources
             else {
@@ -69,43 +60,40 @@ module.exports = function() {
               var proxyActionPermissions = _directory[message.receiver].resources[message.action];
 
               // Gather the permissions required to perform the requested action
+              //
+              // 2015-3-18
+              // There is some conceptual uncertainty here. Would an action intended to be
+              // performed on a database resource ever need more than one permission? If so,
+              // does the proxy agent's permissions need to be identical like with the
+              // previous clause?
               var requiredPermissions = [];
-              if (proxyActionPermissions.read) {
-                requiredPermissions.push('read');
-              }
-              if (proxyActionPermissions.write) {
-                requiredPermissions.push('write');
-              }
-              if (proxyActionPermissions.execute) {
-                requiredPermissions.push('execute');
-              }
+              ['read', 'write', 'execute'].forEach(function(permission) {
+                    if (proxyActionPermissions[permission]) {
+                      requiredPermissions.push(permission);
+                    }
+                });
 
               // Determine if the requesting agent has adequate permission
-              var permitted = false;
               requiredPermissions.forEach(function(permission) {
                     if (verified[permission]) {
                       permitted = true;
                     }
                 });
+            }
 
-              if (permitted) {                
-                // Swap out destination gebo and access token
-                message.gebo = _directory[message.receiver].gebo;
-                message.access_token = _directory[message.receiver].access_token;
-                message.sender = _proxyEmail;
-    
-                performatives.request(message, function(err, results) {
-                      if (err) {
-                        deferred.reject(err);
-                      }
-                      else {
-                        deferred.resolve(results);
-                      }
-                  });
-              }
-              else {
-                deferred.reject('You are not permitted to request or propose that action');
-              }            
+            // Forward the message if permitted
+            if (permitted) {
+              performatives.request(message, function(err, results) {
+                    if (err) {
+                      deferred.reject(err);
+                    }
+                    else {
+                      deferred.resolve(results);
+                    }
+                });
+            }
+            else {
+              deferred.reject('You are not permitted to request or propose that action');
             }
           }
           else {
